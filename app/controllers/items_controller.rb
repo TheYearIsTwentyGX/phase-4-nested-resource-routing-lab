cae8c20fd7151412312bb60c8db80d74c1347cae
Dylan Hays <twentygx@outlook.com>
2023-05-23 19:55:40 -0500

    commit tree 7aecf48852069be845a38ee262653a92e3323fa8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,48 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :index_not_found_response
+  rescue_from NoMethodError, with: :index_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      if params[:id]
+        items = user.items.find_by!(id: params[:id])
+      else
+        items = user.items
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      item = Item.find_by!(id: params[:id])
+      
+    elsif params[:id]
+      item = Item.find(id: params[:id])
+    else
+      item = Item.all
+    end
+    if !item
+      index_not_found_response
+    end
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find_by!(id: params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  def index_not_found_response
+    render json: { error: "The requested resource could not be found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
